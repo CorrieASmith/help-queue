b19da839e79772dbc9d4f531f08369339442201b
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-28 10:19:06 -0700

    add user model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
+var users = [{
+  name: "Michael",
+  question: "Why is my code not working?",
+  time: "",
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return users;
+  },
 });
