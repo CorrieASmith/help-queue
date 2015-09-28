41144d8500b9be10f7f18aa7fcedcfa7f3abcb7d
Corrie Smith & Michael Summers <student@epicodus.com>
2015-09-28 11:01:57 -0700

    add delete functionality

--- app/components/user-tile.js
+++ app/components/user-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(user) {
+      if (confirm('Are you sure?')) {
+        this.sendAction('destroyUser', user);
+      }
+    }
+  }
 });
--- app/routes/instructor.js
+++ app/routes/instructor.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('user');
+  },
+
+  actions: {
+    destroyUser(user) {
+      user.destroyRecord();
+      this.transitionTo('instructor');
+    }
+  }
 });
