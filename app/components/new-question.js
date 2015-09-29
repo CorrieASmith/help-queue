4390158c2e8d4e8fe38a834f570d9dc014ae7429
Michael Summers & Corrie Smith <student@epicodus.com>
2015-09-29 09:37:56 -0700

    displays what time user submitted question

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -24,7 +24,7 @@ export default Ember.Component.extend({
       var params = {
         name: this.get('name'),
         question: this.get('question'),
-        time: this.get('time') ? this.get('time') : ""
+        time: moment() ? moment().format("h:mm a") : ""
       };
       this.set('condition3', false);
       this.sendAction("save2", params);
